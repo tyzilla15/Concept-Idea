bc592a5c97fa7fbe0e9762a223faf696eccdaba2
tyzilla15 <45183223+tyzilla15@users.noreply.github.com>
2018-11-19 17:16:43 -0500

    Player Movement

---
+++ Concept-Idea/Assets/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed;
+    public Rigidbody MyBody;
+    public float JumpForce;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        MyBody.velocity = new Vector3(x * speed, y * speed, MyBody.velocity.z);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            MyBody.velocity = new Vector3(MyBody.velocity.x, MyBody.velocity.y, JumpForce);
+        }
+	}
+}
