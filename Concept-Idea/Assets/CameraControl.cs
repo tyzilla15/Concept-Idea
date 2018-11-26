49ce725a056e897a5c0f0df5f7410f01155cd70d
tyzilla15 <45183223+tyzilla15@users.noreply.github.com>
2018-11-26 17:12:20 -0500

    Looking & Jumping

---
+++ Concept-Idea/Assets/CameraControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+    public float NodSpeed;
+    public float MaxUp = 60;
+    public float MaxDown = -60;
+
+    float yrotate;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        yrotate = Input.GetAxis("Mouse Y")* NodSpeed;
+        //yrotate = Mathf.Clamp(yrotate, MaxDown, MaxUp);
+        
+        transform.Rotate(yrotate, 0, 0);
+    }
+}
--- Concept-Idea/Assets/PlayerMovement.cs
+++ Concept-Idea/Assets/PlayerMovement.cs
@@ -4,24 +4,76 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-    public float speed;
+    public float Speed;
     public Rigidbody MyBody;
     public float JumpForce;
+    float Jump;
+    public float FallTime;
+    public float TurnSpeed;
+
+    bool CanJump = true;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        Jump = JumpForce;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
-        MyBody.velocity = new Vector3(x * speed, y * speed, MyBody.velocity.z);
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            MyBody.velocity = new Vector3(MyBody.velocity.x, MyBody.velocity.y, JumpForce);
-        }
+        Movement();
+        Turning();
+        Jumping();
+        GravityControl();
+        
+
+
 	}
+
+    void Movement()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        MyBody.velocity = transform.forward * Speed * z + transform.right * Speed * x + transform.up * MyBody.velocity.y;
+    }
+
+    void Turning()
+    {
+
+        float xrotate = Input.GetAxis("Mouse X");
+        transform.Rotate(0, xrotate * TurnSpeed, 0);
+    }
+
+    void Jumping()
+    {
+        
+        if (Input.GetKey(KeyCode.Space) && CanJump == true)
+        {
+            Jump = Jump + Time.deltaTime * 2;
+            Debug.Log("Charging Jump, Current Force is: " + Jump);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space) && CanJump == true)
+        {
+            MyBody.velocity = new Vector3(MyBody.velocity.x, Jump, MyBody.velocity.z);
+            CanJump = false;
+            Jump = JumpForce;
+        }
+    }
+
+    void GravityControl()
+    {
+
+        if (MyBody.velocity.y <= 0)
+        {
+            MyBody.velocity += Vector3.down * Time.deltaTime * FallTime;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            CanJump = true;
+        }
+    }
 }
